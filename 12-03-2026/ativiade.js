865012e43f3dfb2f8c780efc7ad3358732d0c2f5
padwiggers <padwiggers@gmail.com>
2026-03-12 16:28:01 -0300

    atividade js

---
+++ 12-03-2026/ativiade.js
@@ -0,0 +1,108 @@
+/*exercicios 1 */
+
+let numero1 = 10;
+let numero2 = 7;
+
+// Somando as duas variáveis
+let soma = numero1 + numero2;
+
+// Mostrando o resultado no console
+console.log("O resultado da soma é: " + soma);
+
+///////////////////////////////////////////////////////////////////////////
+
+/*exercicios 2 */
+
+let numero1 = 15;
+let numero2 = 5;
+
+// Subtraindo a segunda variável da primeira
+let subtracao = numero1 - numero2;
+
+// Mostrando o resultado no console
+console.log("O resultado da subtração é: " + subtracao);
+
+///////////////////////////////////////////////////////////////////////////
+/*exercicios 3  */
+
+let numero1 = 5;
+let numero2 = 8;
+
+// Multiplicação
+let resultado = numero1 * numero2;
+
+// Mostrando o resultado no console
+console.log("O resultado da multiplicação é: " + resultado);
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/*exercicios 4 */
+
+let numero1 = 20;
+let numero2 = 4;
+
+// Dividindo a primeira variável pela segunda
+let divisao = numero1 / numero2;
+
+// Mostrando o resultado no console
+console.log("O resultado da divisão é: " + divisao);
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/*exercicios 5 */
+
+// Criando duas variáveis
+let numero1 = 10;
+let numero2 = 3;
+
+// Calculando o resto da divisão
+let resto = numero1 % numero2;
+
+// Mostrando o resultado no console
+console.log("O resto da divisão é: " + resto);
+
+/*exercicios 6 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
